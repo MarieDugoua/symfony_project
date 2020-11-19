3c1ec0aaa4292c0d78ea64e8ae9f94238e4355fb
f <f>
2020-11-19 14:51:43 +0100

    update

--- SpiceGirls/src/Controller/SpiceGirlsController.php
+++ SpiceGirls/src/Controller/SpiceGirlsController.php
@@ -4,8 +4,13 @@ namespace App\Controller;
 
 use App\Entity\Offers;
 use App\Repository\OffersRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use http\Env\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SpiceGirlsController extends AbstractController
@@ -38,21 +43,45 @@ class SpiceGirlsController extends AbstractController
     /**
      * @Route("/add", name="add")
      */
-    public function add(Offers $offer)
+    public function add(Offers $offer, Request $request, EntityManagerInterface $em)
     {
         $addOffer = new Offers();
         $form = $this->createFormBuilder($addOffer)->getForm()
-            ->add("Titre", )
-            ->add("Description")
+            ->add("Titre", TextType::class, [
+                "attr" => ["class" => "form-control"]
+            ])
+            ->add("Description", TextareaType::class, [
+                "attr" => ["class" => "form-control"]
+            ])
             ->add("Contrat")
-            ->add("Style de conrat")
-            ->add("Adresse")
-            ->add("Code Postal")
-            ->add("Ville")
-            ->add("Date de fin de contrat")
-            ->add('Poster', SubmitType::class);
+            ->add("Type de conrat")
+            ->add("Adresse", TextareaType::class, [
+                "attr" => ["class" => "form-control"]
+            ])
+            ->add("Code Postal", TextareaType::class, [
+                "attr" => ["class" => "form-control"]
+            ])
+            ->add("Ville", TextareaType::class, [
+                "attr" => ["class" => "form-control"]
+            ])
+            ->add("Date de fin de contrat", DateType::class, [
+                "attr" => ["class" => "form-control"]
+            ])
+            ->add('Poster', SubmitType::class, [
+                "attr" => ["class" => "btn btn-outline-primary"]
+            ])
+            ->getForm();
 
-        dd($form);
+        $addOffer->setCreationDate(new \DateTime());
+        $addOffer->setUpdateDate(new \DateTime());
+        $addOffer->setOffers($offer);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $em->persist($addOffer);
+            $em->flush();
+        }
 
         return $this->render('spice/add.html.twig', [
             'offer' => $offer,
